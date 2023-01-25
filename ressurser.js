8dd129dbb7dc5b973c9666bfbc25039b4ccfd015
Christopherhalv0rsen <chrishp1997@hotmail.com>
2023-01-25 13:42:43 +0100

    ..

---
+++ ressurser.js
@@ -0,0 +1,134 @@
+const resources = [
+    {
+        category: "HTML",
+        text: "HTML står for HyperText Markup Language, og er et strukturspråk som brukes for å lage strukturer til nettside- og applikasjonsgrensesnitt.",
+        sources: [
+            {
+                title: "W3Schools",
+                url: "https://www.w3schools.com/html/"
+            },
+            {
+                title: "HTML Living standard",
+                url: "https://html.spec.whatwg.org/multipage/"
+            },
+            {
+                title: "HTML.com Tutorials",
+                url: "https://html.com/"
+            },
+        ]
+    },
+    {
+        category: "CSS",
+        text: "CSS står for Cascading StyleSheets, og brukes for å sette stilregler på HTML-elementer.",
+        sources: [
+            {
+                title: "W3Schools",
+                url: "https://www.w3schools.com/css/"
+            },
+            {
+                title: "W3C HTML & CSS Standards",
+                url: "https://www.w3.org/standards/webdesign/htmlcss.html"
+            },
+            {
+                title: "W3C CSS Validator",
+                url: "https://jigsaw.w3.org/css-validator/"
+            },
+            {
+                title: "CSS Tricks",
+                url: "https://css-tricks.com/"
+            },
+        ]
+    },
+    {
+        category: "JavaScript",
+        text: "JavaScript er et scriptspråk basert på EcmaScript. JavaScript kjører direkte i nettleseren, og brukes ofte til å manipulere HTML og CSS i webgrensnesnitt.",
+        sources: [
+            {
+                title: "W3Schools",
+                url: "https://www.w3schools.com/js/"
+            },
+            {
+                title: "MDN Web Docs",
+                url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+            },
+            {
+                title: "How to read JavaScript Documentation",
+                url: "https://www.youtube.com/watch?v=O3iR-CIufKM"
+            },
+        ]
+    },
+    {
+        category: "React",
+        text: "React er et rammeverk bygget i JavaScript. React bruker komponenter og states for å lage en levende frontend.",
+        sources: [
+            {
+                title: "React documentation",
+                url: "https://reactjs.org/docs/getting-started.html"
+            },
+            {
+                title: "W3Schools",
+                url: "https://www.w3schools.com/REACT/DEFAULT.ASP"
+            },
+            {
+                title: "How to read JavaScript Documentation",
+                url: "https://www.youtube.com/watch?v=O3iR-CIufKM"
+            },
+        ]
+    },
+    {
+        category: "Sanity and headless CMS",
+        text: "Sanity er et headless CMS som står for innholdsadministrasjon. Innhold hentes inn i applikasjoner via GROQ-spørringer.",
+        sources: [
+            {
+                title: "Sanity documentation",
+                url: "https://www.sanity.io/docs"
+            },
+            {
+                title: "OnCrawl: a beginners guide to headless CMS",
+                url: "https://www.oncrawl.com/technical-seo/beginners-guide-headless-cms/"
+            },
+            {
+                title: "Section.io: Getting started with Sanity CMS",
+                url: "https://www.section.io/engineering-education/getting-started-with-sanity-cms/"
+            },
+        ]
+    },
+]
+
+const menu = document.getElementsByClassName("toggle")
+const ider = new Array ("toggle1","toggle2","toggle3","toggle4","toggle5")
+let menuHTML = document.getElementsByClassName("product-card")[0].innerHTML
+
+function endrekategori(id) {
+
+    menu[0].classList.remove("active")
+    menu[1].classList.remove("active")
+    menu[2].classList.remove("active")
+    menu[3].classList.remove("active")
+    menu[4].classList.remove("active")
+let knapp = document.getElementById(id)
+    knapp.classList.add("active")
+
+
+
+let index = ider.indexOf(id)
+
+let knapptekst = resources[index]
+
+menuHTML = ""
+    menuHTML += (`${"<h3>"}${knapptekst.category}${"</h3>"}${"<p>"}${knapptekst.text}${"</p>"}${"<ul>"}`)  
+    knapptekst.sources.map (link => {
+     
+    menuHTML +=  `${"<li>"} ${"<a href ="}${link.url}${">"}${link.title}${"</a>"}${ "</li>"}`
+    })
+    menuHTML += `${"</ul>"}`
+
+    document.getElementsByClassName("product-card")[0].innerHTML = menuHTML
+    
+    
+}             
+
+endrekategori("toggle1") 
+
+  
+
